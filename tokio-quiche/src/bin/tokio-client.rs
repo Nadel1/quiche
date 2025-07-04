f559baae6b1c5e437eb672b092b488a61ee9476b
Natalia Piotrowski <n.piotrowski@tum.de>
2025-07-04 14:22:05 +0200

    change cc algo

--- tokio-quiche/src/bin/tokio-client.rs
+++ tokio-quiche/src/bin/tokio-client.rs
@@ -55,6 +55,7 @@ async fn main() -> tokio_quiche::QuicResult<()> {
     params.settings.initial_rtt = Some(conn_args.initial_rtt);
     params.settings.max_idle_timeout =
         Some(Duration::from_millis(conn_args.idle_timeout));
+    params.settings.cc_algorithm=conn_args.cc_algorithm;
     let (h3_driver, mut controller) =
         ClientH3Driver::new(Http3Settings::default());
     let socket = socket.try_into()?;
