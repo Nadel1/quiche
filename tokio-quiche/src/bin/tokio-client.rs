edac946649d4ba209ae0ccf06faa3dfb6af51eca
Natalia Piotrowski <n.piotrowski@tum.de>
2025-09-18 14:26:11 +0200

    print out received response

--- tokio-quiche/src/bin/tokio-client.rs
+++ tokio-quiche/src/bin/tokio-client.rs
@@ -98,10 +98,10 @@ async fn main() -> tokio_quiche::QuicResult<()> {
                                     "fin" => fin,
                                     "len" => pooled.len()
                                 );
-                                //println!(
-                                //    "{}",
-                                //    std::str::from_utf8(&pooled).unwrap()
-                                //);
+                                println!(
+                                    "{}",
+                                    std::str::from_utf8(&pooled).unwrap()
+                                );
                                 if fin {
                                     println!("received full body, exiting");
                                     break 'body;
