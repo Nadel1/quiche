245141d0e1541bcda6ef7cf6b23b94c3396b1ccb
Natalia Piotrowski <n.piotrowski@tum.de>
2026-06-03 14:56:16 +0200

    remove print

--- tokio-quiche/src/bin/tokio-client.rs
+++ tokio-quiche/src/bin/tokio-client.rs
@@ -115,8 +115,7 @@ async fn main() -> tokio_quiche::QuicResult<()> {
                     println!("fin received");
                     break;
                 },
-                ClientH3Event::Core(event) =>
-                    println!("received event: {event:?}"),
+                ClientH3Event::Core(event) =>{},
                 ClientH3Event::NewOutboundRequest {
                     stream_id,
                     request_id,
